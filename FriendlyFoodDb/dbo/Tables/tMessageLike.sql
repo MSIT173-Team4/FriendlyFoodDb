ebdc3ce684e212b488da25c5daed346025951ccd
408262090 <qsceszqsceszqscesz1@gmail.com>
2026-09-11 15:37:56 +0800

    加入資料表tPostLike、tMessageLike

---
+++ FriendlyFoodDb/dbo/Tables/tMessageLike.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tMessageLike]
+(
+	[fMessageID] INT NOT NULL, 
+    [fUserId] INT NOT NULL,
+	CONSTRAINT [PK_tMessageLike]
+        PRIMARY KEY ([fMessageID], [fUserId]),
+
+    CONSTRAINT [FK_tMessageLike_tMessageTable]
+        FOREIGN KEY ([fMessageID])
+        REFERENCES [dbo].[tMessageTable] ([fMessageID]),
+
+    CONSTRAINT [FK_tMessageLike_tUser]
+        FOREIGN KEY ([fUserId])
+        REFERENCES [dbo].[tUser] ([fId])
+)
