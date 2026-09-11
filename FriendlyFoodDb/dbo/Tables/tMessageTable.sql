5e56ca80d12ce081203067e6dba8e46cf6d17d86
408262090 <qsceszqsceszqscesz1@gmail.com>
2026-09-11 10:45:15 +0800

    刪除Views

--- FriendlyFoodDb/dbo/Tables/tMessageTable.sql
+++ FriendlyFoodDb/dbo/Tables/tMessageTable.sql
@@ -5,7 +5,6 @@
     [fReplyMessageID] INT            NOT NULL,
     [fMessageContent] NVARCHAR (MAX) NOT NULL,
     [fLikes]          INT            CONSTRAINT [DF_MessageTable_fLikes] DEFAULT ((0)) NOT NULL,
-    [fViews]          INT            CONSTRAINT [DF_MessageTable_fViews] DEFAULT ((0)) NOT NULL,
     [fMessageDate]    DATETIME       NOT NULL,
     [fMessageState]   TINYINT        CONSTRAINT [DF_MessageTable_fMessageState] DEFAULT ((1)) NOT NULL,
     CONSTRAINT [PK_MessageTable] PRIMARY KEY CLUSTERED ([fMessageID] ASC),
