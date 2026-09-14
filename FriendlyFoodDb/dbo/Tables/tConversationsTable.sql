7f462556f849f68d89bf413524a196fad74403a4
408262090 <qsceszqsceszqscesz1@gmail.com>
2026-09-14 14:38:28 +0800

    加入tPostBlockTable，刪除PostContent欄位，時間相關欄位的資料型別全部改為datetime2(0)

--- FriendlyFoodDb/dbo/Tables/tConversationsTable.sql
+++ FriendlyFoodDb/dbo/Tables/tConversationsTable.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[tConversationsTable] (
     [fConversationId] INT      IDENTITY (1, 1) NOT NULL,
-    [fCreatedDate]    DATETIME NOT NULL,
-    [fUpdatedDate]    DATETIME NOT NULL,
+    [fCreatedDate]    DATETIME2(0) NOT NULL DEFAULT GETDATE(),
+    [fUpdatedDate]    DATETIME2(0) NOT NULL DEFAULT GETDATE(),
     CONSTRAINT [PK_fConversationsTable] PRIMARY KEY CLUSTERED ([fConversationId] ASC)
 );
 
