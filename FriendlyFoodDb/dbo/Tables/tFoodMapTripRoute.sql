b8b8d3d0cb555e84e9e4eb8feaa7188f52786776
威晨 劉 <week861011@gmail.com>
2026-09-23 23:46:13 +0800

    修改 tfoodmapplace  fplacecategory 、 tfoodmaptrip fUserId  =>  nullable 增加 tfoodmapTripRoute 欄位  fFromTripPlaceId 、 fToTripPlaceID  、 新增外鍵

--- FriendlyFoodDb/dbo/Tables/tFoodMapTripRoute.sql
+++ FriendlyFoodDb/dbo/Tables/tFoodMapTripRoute.sql
@@ -1,16 +1,20 @@
 ﻿CREATE TABLE [dbo].[tFoodMapTripRoute] (
     [fTripRouteID]   INT             IDENTITY (1, 1) NOT NULL,
     [fTripID]        INT             NOT NULL,
-    [fTotalDistance] DECIMAL (10, 2) NULL,
-    [fTotalDuration] INT             NULL,
+    [fDistanceMeters] INT NULL,
+    [fDurationSeconds] INT             NULL,
     [fPolyline]      NVARCHAR (MAX)  NULL,
     [fRouteProvider] VARCHAR (30)    NULL,
-    [fRouteVersion]  INT             CONSTRAINT [DF_tFoodMapTripRoute_fRouteVersion] DEFAULT ((1)) NOT NULL,
+    [fRouteVersion]  INT             CONSTRAINT [DF_tFoodMapTripRoute_fRouteVersion] DEFAULT ((1)) NULL,
     [fCreatedTime]   DATETIME2 (0)   CONSTRAINT [DF_tFoodMapTripRoute_fCreatedTime] DEFAULT (sysdatetime()) NOT NULL,
     [fUpdatedTime]   DATETIME2 (0)   NULL,
+    [fFromTripPlaceID] INT NULL, 
+    [fToTripPlaceID] INT NULL, 
     CONSTRAINT [PK_tFoodMapTripRoute] PRIMARY KEY CLUSTERED ([fTripRouteID] ASC),
-    CONSTRAINT [CK_tFoodMapTripRoute_fTotalDistance] CHECK ([fTotalDistance] IS NULL OR [fTotalDistance]>=(0)),
-    CONSTRAINT [CK_tFoodMapTripRoute_fTotalDuration] CHECK ([fTotalDuration] IS NULL OR [fTotalDuration]>=(0)),
-    CONSTRAINT [FK_tFoodMapTripRoute_fTrip] FOREIGN KEY ([fTripID]) REFERENCES [dbo].[tFoodMapTrip] ([fTripID]) ON DELETE CASCADE
+    CONSTRAINT [CK_tFoodMapTripRoute_fTotalDistance] CHECK ([fDistanceMeters] IS NULL OR [fDistanceMeters]>=(0)),
+    CONSTRAINT [CK_tFoodMapTripRoute_fTotalDuration] CHECK ([fDurationSeconds] IS NULL OR [fDurationSeconds]>=(0)),
+    CONSTRAINT [FK_tFoodMapTripRoute_fTrip] FOREIGN KEY ([fTripID]) REFERENCES [dbo].[tFoodMapTrip] ([fTripID]) ON DELETE CASCADE,
+    CONSTRAINT [FK_tFoodMapTripRoute_fFromTripPlace] FOREIGN KEY ([fFromTripPlaceID]) REFERENCES tFoodMapTripPlace(fTripPlaceId),
+    CONSTRAINT [FK_tFoodMapTripRoute_fToTripPlace] FOREIGN KEY ([fToTripPlaceID]) REFERENCES tFoodMapTripPlace(fTripPlaceId)
 );
 
