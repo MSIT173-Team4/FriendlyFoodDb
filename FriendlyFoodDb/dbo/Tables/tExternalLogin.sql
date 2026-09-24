51ae603e3acfc3f55079121de75ca9ebb36b54d4
Ginny <happierdemon@gmail.com>
2026-09-24 10:33:06 +0800

    新增tExternalLogin

---
+++ FriendlyFoodDb/dbo/Tables/tExternalLogin.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[tExternalLogin]
+(
+	[fId] INT NOT NULL PRIMARY KEY Identity,
+	[fUserId] INT NOT NULL,
+	[fProvider] varchar(50) not null,
+	[fProviderUserID] varchar(max) not null,
+	constraint FK_ExternalLogin_User foreign key (fUserId) references tUser(fId),
+	constraint UQ_ExternalLogin unique (fProvider,fProviderUserId)
+)
